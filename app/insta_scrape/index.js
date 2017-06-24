0aa2cd62d20fa966c1805f1dc386c4cd0c64b192
henrymunro <henrymunro73@gmail.com>
2017-06-24 22:49:19 +0100

    added total count to initial console log

--- app/insta_scrape/index.js
+++ app/insta_scrape/index.js
@@ -1,10 +1,49 @@
 const { scrapeHashtags } = require('./local_modules/top_level_scrape')
 
+const _randNumGen = (min, max) => {
+	return Math.floor(Math.random() * (max - min)) + 1 + min 
+}
+
 const hashtags = [
-	// {hashtag: 'travel', probability:0.9, count:69, userCount: 19, userProbability: 0.5, userPhotoCount: 9},
-	// {hashtag: 'bikeTour', probability:0.85, count:23, usercount: 5, userProbability: 0.5, userPhotoCount: 9},
-	{hashtag: 'nature', probability:0.8, count:46, usercount: 9, userProbability: 0.5, userPhotoCount: 9}
+	{
+		hashtag: 'travel', 
+		probability:0.9, 
+		count:_randNumGen(50, 70), 
+		userCount: _randNumGen(15, 25), 
+		userProbability: 0.5, 
+		userPhotoCount: _randNumGen(6, 10)
+	},
+	{
+		hashtag: 'bikeTour', 
+		probability:0.85, 
+		count:_randNumGen(20, 30), 
+		userCount: _randNumGen(4, 10), 
+		userProbability: 0.5, 
+		userPhotoCount: _randNumGen(6, 10)
+	},
+	{
+		hashtag: 'nature', 
+		probability:0.8, 
+		count:_randNumGen(30, 50), 
+		userCount: _randNumGen(6, 20), 
+		userProbability: 0.5, 
+		userPhotoCount: _randNumGen(6, 10)
+	},	
+	{
+		hashtag: 'sunset', 
+		probability:0.8, 
+		count:_randNumGen(30, 50), 
+		userCount: _randNumGen(8, 15), 
+		userProbability: 0.5, 
+		userPhotoCount: _randNumGen(6, 10)
+	}
 ]
 
+
+const total = hashtags.reduce((acc, val)  => {
+	return acc + val.count + (val.userCount * val.userPhotoCount)
+}, 0)
+
+console.log(`TOTAL TO INSPECT: ${total}`)
 
 scrapeHashtags({hashtags})
